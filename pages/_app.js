7bd31d8eef28585c61093d8c48171c67bf20c82f
Илья Афендин <rozkovdmitrij103@gmail.com>
2021-10-13 20:26:27 +1100

    Inital commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/600.css";
+import "../styles/globals.css";
+import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
